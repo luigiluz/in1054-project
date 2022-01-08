fab33d8731d86138d3d5268ff761b87c55a8d226
Luigi Luz <luigiluz98@gmail.com>
2022-01-07 22:03:07 -0300

    Update preprocessing script

--- scripts/preprocessing_script.py
+++ scripts/preprocessing_script.py
@@ -3,10 +3,13 @@ import pandas as pd
 import in1054.preprocessing
 import in1054.constants as consts
 
+filepath = consts.ROOT_PATH + "/data/DoS_dataset.csv"
+output_filepath = consts.ROOT_PATH + "/data/joinedDoS_dataset.csv"
+
 def main():
-	features_df = pd.read_csv(consts.NORMAL_RUN_DATA_CSV_PATH)
+	features_df = pd.read_csv(filepath, names=consts.COLUMNS_NAMES)
 	print(features_df.head(10))
-	in1054.preprocessing.preprocess(features_df, consts.NORMAL_RUN_DATA_JOINED_PATH)
+	in1054.preprocessing.preprocess(features_df, output_filepath)
 
 if __name__ == "__main__":
 	main()
