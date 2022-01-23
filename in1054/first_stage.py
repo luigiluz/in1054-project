00c936e7f169e1ada699e6c7876a4f63c6e52620
Luigi Luz <luigiluz98@gmail.com>
2022-01-23 03:36:19 -0300

    Update first stage eval script

--- in1054/first_stage.py
+++ in1054/first_stage.py
@@ -1,5 +1,5 @@
 import pybloomfilter
-import pybloom
+import pybloom_live as pybloom
 
 import in1054.constants as consts
 
@@ -20,12 +20,15 @@ def _convert_bloom_filter_results(results):
 
 
 def povoate_bloom_filter(normal_dataframe, bf_filename=None):
-	concatenated_features_df = normal_dataframe.copy()
-	concatenated_features_array = concatenated_features_df.loc[:, consts.CONCATENATED_FEATURES_COLUMN_NAME].values
+	tmp_df = normal_dataframe.copy()
+	concatenated_features_array = tmp_df.loc[:, consts.CONCATENATED_FEATURES_COLUMN_NAME].values
+
+	n_of_rows = concatenated_features_array.shape[0]
+	bf_capacity = int(n_of_rows + 0.1 * n_of_rows)
 
 	# TODO: Update filter capacity based on the amount of data used to create
 	# normal state representation
-	fs_bloomfilter = pybloomfilter.BloomFilter(capacity=10000000, error_rate=0.01, filename=bf_filename)
+	fs_bloomfilter = pybloomfilter.BloomFilter(capacity=bf_capacity, error_rate=0.01, filename=bf_filename)
 
 	for feature in concatenated_features_array:
 		fs_bloomfilter.add(feature.encode())
@@ -54,6 +57,9 @@ def povoate_pybloom_filter(normal_dataframe):
 	concatenated_features_df = normal_dataframe.copy()
 	concatenated_features_array = concatenated_features_df.loc[:, consts.CONCATENATED_FEATURES_COLUMN_NAME].values
 
+	n_of_rows = concatenated_features_array.shape[0]
+	bf_capacity = int(n_of_rows + 0.1 * n_of_rows)
+
 	pybloom_bloomfilter = pybloom.ScalableBloomFilter(mode=pybloom.ScalableBloomFilter.SMALL_SET_GROWTH, error_rate=0.001)#BloomFilter(capacity=1000000, error_rate=0.01)
 
 	for feature in concatenated_features_array:
@@ -73,6 +79,21 @@ def check_pybloomfilter(eval_dataframe, pybloomfilter):
 	for word in words_array:
 		single_result = word in pybloomfilter
 		bf_results.append(single_result)
+
+	results_as_int = _convert_bloom_filter_results(bf_results)
+
+	return results_as_int
+
+
+def check_pybloomfilter_single(single_value, pybloomfilter):
+
+	word = single_value.values
+	word = word.tolist()
+
+	bf_results = []
+
+	single_result = word in pybloomfilter
+	bf_results.append(single_result)
 
 	results_as_int = _convert_bloom_filter_results(bf_results)
 
--- scripts/first_stage_eval_script.py
+++ scripts/first_stage_eval_script.py
@@ -1,52 +1,85 @@
 import pandas as pd
+import pickle
+import numpy as np
+import tensorflow as tf
 
 import in1054.constants as consts
 import in1054.preprocessing as preprocessing
 import in1054.first_stage as first_stage
 import in1054.parser as parser
 import in1054.metrics as metrics
+import in1054.utils as utils
+import in1054.second_stage as second_stage
+
+data_folder_dir = consts.ROOT_PATH + "/data/"
+kind_of_data_prefix = "RAND_nokf_noPCA"
+train_filename = data_folder_dir + kind_of_data_prefix + "_train_DoS_dataset.csv"
+validation_filename = data_folder_dir + kind_of_data_prefix + "_validation_DoS_dataset.csv"
 
 def main():
-	# load normal dataframe
-	normal_joined_df = pd.read_csv(consts.NORMAL_RUN_DATA_JOINED_PATH)
-	print("normal_joined_df")
-	print(normal_joined_df.head(10))
+	# Load train dataset
+	train_df = pd.read_csv(train_filename)
+	train_df = train_df.drop(columns="index")
 
-	# load malicious dataframe
-	dos_df = pd.read_csv(consts.DOS_DATA_CSV_PATH, names=consts.COLUMNS_NAMES, index_col=0)
-	# filter dlc columns
-	dos_df = preprocessing.filter_by_dlc(dos_df, 8)
-	print("dos_df")
-	print(dos_df.head(10))
+	# Load validation dataset
+	# Used for finding optimal hyper-parameters
+	validation_df = pd.read_csv(validation_filename)
+	validation_df = validation_df.drop(columns=["index", "level_0"])
 
-	dos_results_column_df = dos_df[[consts.FLAG_COLUMN_NAME]]
-	print("dos_results_column_df")
-	print(dos_results_column_df.head(10))
+	# Initiliaze training
+	training_data = train_df
+	validation_data = validation_df
 
-	dos_results_column_df = preprocessing.convert_results_to_int(dos_results_column_df)
-	print("dos_results_column_df_after_conversion")
-	print(dos_results_column_df.head(10))
-	dos_results = dos_results_column_df.loc[:, consts.FLAG_COLUMN_NAME].tolist()
+	random_seed = 1
+	# Setting random seeds
+	np.random.seed(random_seed)
+	tf.random.set_seed(random_seed)
 
-	# preprocess malicious dataframe
-	dos_df = parser.convert_cols_from_hex_string_to_int(dos_df, consts.COLUMNS_TO_CONVERT)
-	dos_df = preprocessing.preprocess(dos_df)
-	print("preprocessed_df")
-	print(dos_df)
+	training_data_features, training_data_labels = utils.split_features_and_labels(training_data)
+	validation_data_features, validation_data_labels = utils.split_features_and_labels(validation_data)
+
+	train_features_np = second_stage.convert_df_to_numpy_array(training_data_features)
+	validation_features_np = second_stage.convert_df_to_numpy_array(validation_data_features)
+
+	training_data_labels_np = training_data_labels.to_numpy()
+	validation_data_labels_np = validation_data_labels.to_numpy()
+
+	# If using text vectorizer, this needs to be changed
+	binary_train_features = train_features_np
+	binary_validation_features = validation_features_np
+
+	# Juntar os dados numa coluna unica
+	converted_array = preprocessing.convert_to_comma_separated_string(binary_train_features)
+	concatenated_train_features_df = pd.DataFrame(converted_array, columns = [consts.CONCATENATED_FEATURES_COLUMN_NAME])
+
+	converted_val_array = preprocessing.convert_to_comma_separated_string(binary_validation_features)
+	concatenated_validation_features_df = pd.DataFrame(converted_val_array, columns = [consts.CONCATENATED_FEATURES_COLUMN_NAME])
 
 	# povoate bloom filter
-	print("povoating bloom filter")
-	fs_bloomfilter = first_stage.povoate_bloom_filter(normal_joined_df)
+	# print("povoating bloom filter")
+	# fs_bloomfilter = first_stage.povoate_pybloom_filter(concatenated_train_features_df)
 
-	# get bloom filter results
-	bf_results = first_stage.check_bloomfilter(dos_df, fs_bloomfilter)
+	filename = "bloom_filter.pickle"
+
+	# # Save bloom filter as a pickle model
+	# with open(filename ,'wb') as file:
+	# 	pickle.dump(fs_bloomfilter, file)
+
+	infile = open(filename, 'rb')
+	loaded_bloomfilter = pickle.load(infile)
+	infile.close()
+
+	print(loaded_bloomfilter)
+
+	# # get bloom filter results
+	bf_results = first_stage.check_bloomfilter(concatenated_validation_features_df, loaded_bloomfilter)
 
 	# evaluate bloom filter results
-	conf_matrix = metrics.conf_matrix(dos_results, bf_results)
+	conf_matrix = metrics.conf_matrix(validation_data_labels_np, bf_results)
 	print("confusion matrix")
 	print(conf_matrix)
 
-	metrics_df = metrics.classification_metrics(dos_results, bf_results)
+	metrics_df = metrics.classification_metrics(validation_data_labels_np, bf_results)
 	print(metrics_df)
 
 
